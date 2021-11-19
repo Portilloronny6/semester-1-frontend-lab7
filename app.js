0effcf952914578775ada68835d4e5edc5ea7d0f
Ronny Portillo <41457625+Portilloronny6@users.noreply.github.com>
2021-11-19 15:37:44 +0500

    oir tu voz

--- app.js
+++ app.js
@@ -1,8 +1,40 @@
-function checkInp(selector) {
-  let x = document.querySelector(selector).value;
-  if (!/^\d+$/.test(document.querySelector("selector").value)) {
-    alert("Mueranse");
-  }
-}
+const inputs = document.querySelectorAll("[type='number']");
+const total = document.querySelector("#total");
+const buttonSend = document.querySelector("#submit-button");
 
-document.querySelector("#suma").addEventListener("keypress", checkInp("#suma"));
+const inputValidator = {
+  suma: false,
+  terms: false,
+  percentage: false,
+};
+
+inputs.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    let name = e.target.getAttribute("name");
+
+    if (e.target.value.length > 0) {
+      inputValidator[name] = true;
+    } else {
+      inputValidator[name] = false;
+    }
+
+    let allTrue = Object.keys(inputValidator).every((item) => {
+      return inputValidator[item] === true;
+    });
+
+    if (allTrue) {
+      buttonSend.disabled = false;
+    } else {
+      buttonSend.disabled = true;
+    }
+  });
+});
+
+document.querySelector("#form-user").addEventListener("submit", function (e) {
+  const suma = document.querySelector("#suma").value;
+  const terms = document.querySelector("#terms").value;
+  const percentage = document.querySelector("#percentage").value;
+
+  total.value = (suma * (percentage / 100 / 12) * terms).toFixed(3);
+  e.preventDefault();
+});
